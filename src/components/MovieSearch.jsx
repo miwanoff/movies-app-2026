ae083c68bc7349d045842dca895ac375e57c3ab2
miwanoff <marina.iva.noffa@gmail.com>
2026-05-16 12:06:36 +0300

    Add cards

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+const API_URL = "https://www.omdbapi.com?apikey=ee8dd3f";
 
 const MovieSearch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovie("batman");
+  }, []);
+
+  const searchMovie = async (title) => {
+    try {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error searching movies:", error);
+    }
+  };
+  //searchMovie();
   return (
     <div>
-      <h1>MovieSearch </h1>
+      <h2>Movie Search</h2>
     </div>
   );
 };
+
 export default MovieSearch;
